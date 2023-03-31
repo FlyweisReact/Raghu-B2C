3394307bd709b93e2cebd3a42531958af34df503
React1 <React1@flyweis.technology>
2023-03-31 12:35:26 +0530

    changes

---
+++ .history/src/Modals/UserDashModal_20230330184959.js
@@ -0,0 +1,39 @@
+/** @format */
+
+import { useNavigate } from "react-router";
+import { Modal } from "react-bootstrap";
+
+function UserDashModal(props) {
+  const navigate = useNavigate();
+  function SignOut() {
+    localStorage.clear();
+    window.location.reload();
+    alert("You");
+  }
+
+  return (
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Body>
+        <div className="Userinfo">
+          <button type="button" onClick={() => navigate("/NewCoursePage")}>
+            My Account
+          </button>
+          <button type="button" onClick={() => navigate("/wishlist")}>
+            Wishlist
+          </button>
+          <button type="button" onClick={() => navigate("/cart")}>
+            Go to Cart
+          </button>
+          <a href="/secondCourse#allCoursses">
+            <button type="button">My Courses</button>
+          </a>
+          <button type="button" onClick={() => SignOut()}>
+            Sign out
+          </button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default UserDashModal;
---
+++ .history/src/Screens/Wishlist/Wishlist_20230330192205.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import Header from '../../Header/Header'
+import './Wishlist.css'
+import axios from 'axios'
+
+const Wishlist = () => {
+  const userId = localStorage.getItem("UserId")
+  const token = localStorage.getItem('token')
+  const [ data , setData ] = useState([])
+
+  const getData = useCallback(async () => {
+    try{
+      const { data } = await axios.get(` https://52pv9t2fl3.execute-api.ap-south-1.amazonaws.com/dev/api/v1/wishlist/${userId}`)
+      setData(data.message[0].course)
+    }catch(err){
+      console.log(err)
+    }
+  },[userId])
+
+  useEffect(() => (
+    if(token){
+      getData()
+    }
+  ),[])
+
+  const deleteHandler = async (id) => {
+    try{
+      const data = await axios.put(` https://52pv9t2fl3.execute-api.ap-south-1.amazonaws.com/dev/api/v1/removewishlist/${userId}` , {courseId :  id})
+      console.log(data)
+      alert('Course Removed from Wishlist')
+      getData()
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+  return (
+    <>
+        <Header />
+
+        <div className='wishlist'>
+            <p className='head'> {data?.length} Course in cart</p>
+            {data?.map((i , index) => (
+              <div className='wishlistCart' key={index}>
+            <img src={i.image} alt='CourseImage' />
+            <p className='CourseName'> {i.title} </p>
+            <div className='removeDiv'>
+                <p onClick={() => deleteHandler(i._id)} >Remove</p>
+                <p>Save for Later</p>
+            </div>
+            </div>
+            ))}
+        </div>
+    </>
+  )
+}
+
+export default Wishlist
